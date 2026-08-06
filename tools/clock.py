858a742b49012c774469f3e3bf3e647efbf1c442
Ella Jameson <ellagjameson@gmail.com>
2026-08-06 00:33:27 -0700

    Basic felesian clock

---
+++ tools/clock.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import time
+from modules.feltime import feltime
+
+
+
+print("\n     Felesian Clock")
+print(  " Press Ctrl+C to exit...")
+
+print("\n  +------------------+")
+print("\n  +------------------+")
+print("\033[2A", end="\r")
+try:
+    while True:
+        print(feltime.now().strftime("  | %c |"), end="")
+        print("\033[2B", end="\r")
+        time.sleep(1)
+        print("\033[2A", end="\r")
+except KeyboardInterrupt:
+    print("\033[2A", end="\r")
+    print(feltime.now().strftime("  | %c |"), end="\r")
+    print("\033[2B")
+    print("   Program has exited.")
+    print("    Have a nice day!")
