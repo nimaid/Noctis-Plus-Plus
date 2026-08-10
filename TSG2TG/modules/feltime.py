c62cdef5c0042c49259e0bb7bb5e1208531f8bd8
Ella Jameson <ellagjameson@gmail.com>
2026-08-10 03:06:50 -0700

    Fixed time conversion by porting the buggy code directly from Noctis

--- TSG2TG/modules/feltime.py
+++ TSG2TG/modules/feltime.py
@@ -1,10 +1,12 @@
 import datetime as _dt
 
-DATETIME_EPOCH = _dt.datetime(year=1984, month=1, day=2)
+DATETIME_EPOCH = _dt.datetime(year=1984, month=1, day=1)
 EPOC_EPOCH = 6011.0
 
 EPOC_MIN = 5949
 EPOC_MAX = 9999
+
+_days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 class datetime:
     def __init__(self, epoc, sinister, medius, dexter=0, fraction=0):
@@ -63,8 +65,46 @@ class datetime:
         if not isinstance(dt, _dt.datetime):
             raise TypeError(f"'dt' must be a 'datetime.datetime object', not a '{type(dt).__name__}'")
         
-        epoc_raw = EPOC_EPOCH + ((dt - DATETIME_EPOCH).total_seconds() / 1e9)
-    
+        '''
+        There are various bugs in the time calculation in Noctis. The epoc is
+        supposed to be computed as:
+            epoc = (number_of_seconds_since_1984 / 1e9) + 6011
+        
+        However, due to faulty leap year logic, every year which is not a leap
+        year will actually be 1 day in the past. Then, at the start of the next
+        leap year, it will gain a day to be accurate again, only to lose it
+        after the year ends.
+        
+        To reproduce the behavior of the game as accurately as possible, I have
+        ported the getsecs() function literally. If the time bugs were not
+        present, I could simply do the following instead:
+            epoc_raw = EPOC_EPOCH + ((dt - DATETIME_EPOCH).total_seconds() / 1e9)
+        '''
+        
+        years = dt.year - DATETIME_EPOCH.year  # years since 1984
+        days = (years * 365) + (years // 4)  # years * 365 + years / 4
+        
+        m = 1
+        while m < dt.month:
+            days += _days_per_month[m-1]  # days per month
+            m += 1
+        
+        if (dt.month > 2) and (dt.year % 4 == 0):  # if it's a leap year
+            days += 1
+        
+        days += dt.day - 1  # + current day of the month
+        seconds = days * 86400  # seconds in a day
+        
+        seconds += 3600 * dt.hour  # current hour in seconds
+        seconds += 60 * dt.minute  # current minute in seconds
+
+        seconds += dt.second  # current seconds
+        
+        seconds += dt.microsecond / 1e6  # fraction of a second
+
+        epoc_raw = 6011 + (seconds / 1e9)
+        
+        # Split the raw epoc into parts
         epoc, triad = f"{epoc_raw:.12f}".split(".")
         
         return cls(
@@ -101,7 +141,75 @@ class datetime:
         if not isinstance(epoc_raw, float) and not isinstance(epoc_raw, int):
             raise TypeError(f"'epoc_raw' must be a 'float' or 'int', not a '{type(epoc_raw).__name__}'")
         
-        return DATETIME_EPOCH + _dt.timedelta(seconds=(epoc_raw - EPOC_EPOCH) * 1e9)
+        '''
+        There are various bugs in the time calculation in Noctis. The epoc is
+        supposed to be computed as:
+            epoc = (number_of_seconds_since_1984 / 1e9) + 6011
+        
+        However, due to faulty leap year logic, every year which is not a leap
+        year will actually be 1 day in the past. Then, at the start of the next
+        leap year, it will gain a day to be accurate again, only to lose it
+        after the year ends.
+        
+        To reproduce the behavior of the game as accurately as possible, I have
+        ported the getsecs() function literally. I then had to derive the
+        inverse of that function below. If the time bugs were not present, I
+        could simply do the following instead:
+            dt = DATETIME_EPOCH + _dt.timedelta(seconds=(epoc_raw - EPOC_EPOCH) * 1e9)
+        '''
+        # Seconds since 1984
+        seconds = (epoc_raw - EPOC_EPOCH) * 1e9
+        
+        # Split off the fraction of a second
+        microsecond = int((seconds % 1) * 1e6)
+        seconds = int(seconds)
+        
+        # Split off hour + minute + second and the remaining days component
+        seconds_in_day = seconds % 86400
+        days = seconds // 86400
+        
+        hour = seconds_in_day // 3600
+        minute = (seconds_in_day % 3600) // 60
+        second = seconds_in_day % 60
+        
+        # Split off the year
+        year = DATETIME_EPOCH.year
+        while True:
+            years = (year + 1) - DATETIME_EPOCH.year
+            next_year_days = (years * 365) + (years // 4)
+            if next_year_days > days:
+                break
+            year += 1
+        
+        years = year - DATETIME_EPOCH.year
+        days -= (years * 365) + (years // 4)
+        
+        # Split off the month
+        month = 1
+        while month < 12:
+            next_month_days = _days_per_month[month - 1]
+            
+            if month == 2 and (year % 4 == 0):
+                next_month_days += 1
+                
+            if days >= next_month_days:
+                days -= next_month_days
+                month += 1
+            else:
+                break
+        
+        # Split off the day
+        day = days + 1
+        
+        return _dt.datetime(
+            year=year,
+            month=month,
+            day=day,
+            hour=hour,
+            minute=minute,
+            second=second,
+            microsecond=microsecond,
+        )
     
     def strftime(self, format):
         '''
